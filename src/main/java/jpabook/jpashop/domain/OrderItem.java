c5cdd11a4cad505ae861bc6b0e3b43f44484819d
Lee Donggun <tnpdlzi@naver.com>
2021-03-16 23:27:12 +0900

    Order Entity 구현

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain;
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 
@@ -27,4 +28,29 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
 
+    // == 생성 메서드 == //
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // == 비지니스 로직 == //
+    public void cancel() {
+        getItem().addStock(count); // 재고 수량 원복
+    }
+
+    // == 조회 로직 == //
+
+    /**
+     * 주문 상품 전체 가격 조회
+     */
+    // 주문 가격과 수량을 곱해야 함.
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
