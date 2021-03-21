46ddeaf8709a3ef4a21b8607b2974d280c8b7856
Lee Donggun <tnpdlzi@naver.com>
2021-03-21 21:02:28 +0900

    주문 검색 기능 구현

---
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName; // 회원의 이름
+    private OrderStatus orderStatus; // 주문의 상태 [ORDER, CANCEL]
+
+
+}
